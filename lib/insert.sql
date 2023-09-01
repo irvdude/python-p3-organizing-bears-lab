7bd1a8251b2449c33e2055f1290ab1df70d51a1d
Irvin Dinkins <irvdude44@gmail.com>
2023-09-01 16:25:36 -0400

    passed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "brown", "mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "M", "black", "aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "F", "white", "light", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "brown", "aggressive", 2);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "brown", "light", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 9, "M", "green", "aggressive", 2);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "F", "white", "mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 4, "F", "black", "aggressive", 1);
